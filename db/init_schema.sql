891029be1e396bb4d9a807932a045869e3d9500b
Mac DeCourcy <mac@decourcy.dev>
2022-05-12 09:38:59 -0700

    added updated_at column to data table and added trigger function to update

--- db/init_schema.sql
+++ db/init_schema.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE "sites" (
   id bigint PRIMARY KEY NOT NULL UNIQUE,
   name varchar NOT NULL,
@@ -11,6 +19,7 @@ CREATE TABLE "data" (
   firstshot bigint NOT NULL,
   secondshot bigint NOT NULL,
   created_at timestamptz NOT NULL DEFAULT (now()),
+  updated_at timestamptz NOT NULL DEFAULT (now()),
   unique(site_id, date)
 );
 
@@ -19,3 +28,8 @@ ALTER TABLE data ADD FOREIGN KEY (site_id) REFERENCES sites(id);
 CREATE UNIQUE INDEX ON sites (name);
 CREATE UNIQUE INDEX ON sites (id);
 CREATE UNIQUE INDEX ON data (site_id);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON data
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
